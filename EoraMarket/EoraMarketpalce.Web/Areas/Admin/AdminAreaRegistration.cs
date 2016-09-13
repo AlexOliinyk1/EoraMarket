671139d6a03c2ef82a483a218724235a1f2bfb6b
Matviyuk Serhiy <serhij.matvijuk@gmail.com>
2016-09-13 17:26:36 +0300

    create admin area

---
+++ EoraMarket/EoraMarketpalce.Web/Areas/Admin/AdminAreaRegistration.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+
+namespace EoraMarketpalce.Web.Areas.Admin
+{
+    public class AdminAreaRegistration : AreaRegistration
+    {
+        private readonly string _areaName = "Admin";
+
+        public override string AreaName
+        {
+            get { return _areaName; }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Admin_default",
+                "Admin/{controller}/{action}/{id}",
+                new { controller = "AdminPanel", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ EoraMarket/EoraMarketpalce.Web/Areas/Admin/Controllers/AdminPanelController.cs
@@ -0,0 +1,15 @@
+﻿using EoraMarketpalce.Web.Common.Constants;
+using EoraMarketpalce.Web.Controllers.Base;
+using System.Web.Mvc;
+
+namespace EoraMarketpalce.Web.Areas.Admin.Controllers
+{
+    [Authorize(Roles = AppConsts.AdminRoleName)]
+    public class AdminPanelController : AppController
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
